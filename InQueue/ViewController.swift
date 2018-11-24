62cd7becbd42be1f4bd7da69a53d9a05a564e603
jaebooker <jaesonbooker@gmail.com>
2018-11-23 22:49:20 -0800

    created initial map annotations

--- InQueue/ViewController.swift
+++ InQueue/ViewController.swift
@@ -7,12 +7,30 @@
 //
 
 import UIKit
+import MapKit
+
+final class MapAnnotation: NSObject, MKAnnotation {
+    var coordinate: CLLocationCoordinate2D
+    var title: String?
+    var subtitle: String?
+    
+    init(coordinate: CLLocationCoordinate2D, title: String?, subtitle: String?) {
+        self.coordinate = coordinate
+        self.title = title
+        self.subtitle = subtitle
+        super.init()
+    }
+}
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        mapView.register(MKMarkerAnnotationView.self, forAnnotationViewWithReuseIdentifier: MKMapViewDefaultClusterAnnotationViewReuseIdentifier)
+        let venueCoordinate = CLLocationCoordinate2D(latitude: 37.780664, longitude: -122.416183)
+        let venueAnnotation = MapAnnotation(coordinate: venueCoordinate, title: "Tenderloin", subtitle: "Eaten Them Tender Loins TM")
+        mapView.addAnnotation(venueAnnotation)
     }
 
 
